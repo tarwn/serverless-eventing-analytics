e5e821fbeb700e31518202dc923e55e9f303ffc5
Eli Weinstock-Herman <tarwn@tiernok.com>
2018-01-08 20:27:38 -0500

    Offline stream functions include changes without restarting

--- offlineStreams/runOfflineStreamHandlers.js
+++ offlineStreams/runOfflineStreamHandlers.js
@@ -10,18 +10,53 @@ const kinesis = new AWS.Kinesis({
     apiVersion: '2013-12-02',
     sslEnabled: false
 });
+const functions = [
+    { funName: 'RuleProcessor', handlerPath: '../functions/ruleProcessor/handler', handlerName: 'streamProcessor', kinesisStreamName: process.env.KINESIS_STREAM_NAME_EVENTS },
+    { funName: 'AlertProcessor', handlerPath: '../functions/alertProcessor/handler', handlerName: 'streamProcessor', kinesisStreamName: process.env.KINESIS_STREAM_NAME_ALERTS }
+];
+initialize(functions);
+
+// ---
+
+function initialize(functions) {
+    functions.forEach((f) => { 
+        run(cacheInvalidated(f), { kinesis: kinesis, streamName: f.kinesisStreamName, console: getLog(f.funName) });
+    });
+}
 
 function getLog(functionName) {
     return {
         log: (m) => console.log(`\n${functionName}: ${m}`),
         error: (e) => console.error(`\n${functionName}:`, e.message, e.stack)
     };
-}    
+}
 
-const ruleLambda = require('../functions/ruleProcessor/handler').streamProcessor;
-run(ruleLambda, { kinesis: kinesis, streamName: process.env.KINESIS_STREAM_NAME_EVENTS, console: getLog('RuleProcessor') });
+function cacheInvalidated(funOptions) { 
+    return (kinesisEvent, context, callback) => { 
+        const handler = createHandler(funOptions, {});
+        handler(kinesisEvent, context, callback);
+    };
+}
 
-const alertLambda = require('../functions/alertProcessor/handler').streamProcessor;
-run(ruleLambda, { kinesis: kinesis, streamName: process.env.KINESIS_STREAM_NAME_ALERTS, console: getLog('AlertProcessor') });
+// borrowed from serverless-offline - temporary until I incorporate it into poller
+function createHandler(funOptions, options) {
+    if (!options.skipCacheInvalidation) {
+      //debugLog('Invalidating cache...');
 
+      for (const key in require.cache) {
+        // Require cache invalidation, brutal and fragile.
+        // Might cause errors, if so please submit an issue.
+        if (!key.match('node_modules')) delete require.cache[key];
+      }
+    }
+
+    //debugLog(`Loading handler... (${funOptions.handlerPath})`);
+    const handler = require(funOptions.handlerPath)[funOptions.handlerName];
+
+    if (typeof handler !== 'function') {
+      throw new Error(`Serverless-offline: handler for '${funOptions.funName}' is not a function`);
+    }
+
+    return handler;
+}
 
